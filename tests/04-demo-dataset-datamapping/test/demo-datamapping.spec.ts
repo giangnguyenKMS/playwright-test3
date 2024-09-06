479150d099edf871b80354091ba4133d28b1c90c
giangnguyenKMS <giangtnguyen@kms-technology.com>
2024-09-06 20:11:43 +0700

    Update workflow

--- tests/04-demo-dataset-datamapping/test/demo-datamapping.spec.ts
+++ tests/04-demo-dataset-datamapping/test/demo-datamapping.spec.ts
@@ -1,33 +1,33 @@
-import { test } from "../fixture/my-fixture";
-import { productNameMapping } from "../utils/data-mapping/map";
-const dataset = JSON.parse(JSON.stringify(require("../data/placeOrderTestDataMapping.json")));
-console.log(dataset);
+// import { test } from "../fixture/my-fixture";
+// import { productNameMapping } from "../utils/data-mapping/map";
+// const dataset = JSON.parse(JSON.stringify(require("../data/placeOrderTestDataMapping.json")));
+// console.log(dataset);
 
-for (const data of dataset) {
-  test("abc", {
-    tag: [
-      '@demo_data_mapping',
-      '@smoke',
-    ]
-  }, async({
-    dashBoardPage, 
-    cartPage, 
-  }) => {
-    const email = data.username;
-    const productName = <string>productNameMapping.get(data.fieldName);
-    const dashBoardPageUrl = data.dashBoardPageUrl;
-    const countryName = data.countryName;
-    const orderSuccessfullyMessage = data.orderMessage;
+// for (const data of dataset) {
+//   test("abc", {
+//     tag: [
+//       '@demo_data_mapping',
+//       '@smoke',
+//     ]
+//   }, async({
+//     dashBoardPage, 
+//     cartPage, 
+//   }) => {
+//     const email = data.username;
+//     const productName = <string>productNameMapping.get(data.fieldName);
+//     const dashBoardPageUrl = data.dashBoardPageUrl;
+//     const countryName = data.countryName;
+//     const orderSuccessfullyMessage = data.orderMessage;
     
-    await dashBoardPage.goToDashBoardPage(dashBoardPageUrl);
-    await dashBoardPage.searchProductAddCart(productName);
-    await dashBoardPage.navigateToCart();
+//     await dashBoardPage.goToDashBoardPage(dashBoardPageUrl);
+//     await dashBoardPage.searchProductAddCart(productName);
+//     await dashBoardPage.navigateToCart();
   
-    await cartPage.VerifyProductIsDisplayed(productName);
-    await cartPage.Checkout();
-    await cartPage.selectCountry(countryName);
-    await cartPage.verifyEmailDisplayCorrectly(email);
-    await cartPage.clickPlaceOrderButton();
-    await cartPage.verifyOrderSuccessfully(orderSuccessfullyMessage);
-  });
-}
+//     await cartPage.VerifyProductIsDisplayed(productName);
+//     await cartPage.Checkout();
+//     await cartPage.selectCountry(countryName);
+//     await cartPage.verifyEmailDisplayCorrectly(email);
+//     await cartPage.clickPlaceOrderButton();
+//     await cartPage.verifyOrderSuccessfully(orderSuccessfullyMessage);
+//   });
+// }
